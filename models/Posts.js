efebec79f0958a3f36191b3bac100a472cef592b
ccaitano <cheryl.caitano@gmail.com>
2022-09-04 16:05:38 -1000

    Start Posts model

---
+++ models/Posts.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    upload_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    user_id: {
+    //   add FOREIGN KEY
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Gallery;

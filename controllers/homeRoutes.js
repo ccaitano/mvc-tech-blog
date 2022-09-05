f47bd9bb108e29e69ddece53cf362106fbaeda63
ccaitano <cheryl.caitano@gmail.com>
2022-09-04 17:21:14 -1000

    Add in authentication to redirect for login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,11 @@
-// Route "/"
+const router = require('express').Router();
+const { User, Posts, Comments } = require('../models');
+const withAuth = require('../utils/auth');
 
+// Route "/"
+router.get('/', async (req, res) => {
+
+})
 // Route "/login"
 
 // Route "/dashboard"

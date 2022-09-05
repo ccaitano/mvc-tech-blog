bc810001b74ddc118557c21f0bb09858308874de
ccaitano <cheryl.caitano@gmail.com>
2022-09-04 17:34:20 -1000

    Add login/session code to homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,11 +3,33 @@ const { User, Posts, Comments } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Route "/"
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+        });
 
-})
+        const users = userData.map((project) => project.get({ plain: true }));
+
+        res.render('homepage', {
+            users,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // Route "/login"
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
 
+    res.render('login');
+});
 // Route "/dashboard"
 
 // Route "/dashboard/new"
@@ -15,3 +37,5 @@ router.get('/', async (req, res) => {
 // Route "/dashboard/edit/:id"
 
 // Route "/post/:id"
+
+module.exports = router;
